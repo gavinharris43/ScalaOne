84b42924ffbb5cba5778316272b12a009107d131
Gavin Harris <gavinharris43@googlemail.com>
2020-03-04 10:02:40 +0000

    fixed bugs

--- src/main/scala/day2/day2Problem.scala
+++ src/main/scala/day2/day2Problem.scala
@@ -12,32 +12,30 @@ object day2Problem extends App {
 
   def runHangman(): Unit = {
     val word = getRandomWord
-    var lives = 11;
-    var wordGuess = "_"*word.length
+    var lives = 7;
+    var wordGuess = "_" * word.length
 
-    while (lives >= 0){
-
+    while (lives >= 0) {
       if (lives == 0) {
         println("You Have Been Hung")
-        lives-=1
+        println(s"The Word Was $word")
+        lives -= 1
       } else {
         println("Guess Letter")
-        val input :Char = StdIn.readChar()
-        if (!guessLetter(word, input)){
+        val input: Char = StdIn.readChar()
+        if (!guessLetter(word, input)) {
           lives -= 1
-          println("lives Remaining "+ lives)
-        } else{
-         wordGuess= wordGuess.updated(word.indexOf(input),input)
-         var lastArray= findNext(word, input)
-
+          println("lives Remaining " + lives)
+        } else {
+          wordGuess = wordGuess.updated(word.indexOf(input), input)
+          var lastArray = findNext(word, input)
           while (lastArray != -1) {
-
             wordGuess = wordGuess.updated(lastArray, input)
-            lastArray= word.indexOf(lastArray+1)
+            lastArray = word.indexOf(lastArray + 1)
           }
           println(wordGuess)
           println("correct")
-          if(areWinner(wordGuess)){
+          if (areWinner(wordGuess)) {
             println(s"Lives Left: $lives")
             println("You Have Won Congratulations!!! \n You have not been sentenced to death my hanging.")
             lives = -1
@@ -46,8 +44,9 @@ object day2Problem extends App {
       }
     }
   }
-  def findNext(word:String, input :Char): Int ={
-    word.indexOf(input, word.indexOf(input)+1)
+
+  def findNext(word: String, input: Char): Int = {
+    word.indexOf(input, word.indexOf(input) + 1)
   }
 
   def readFile = {
@@ -56,10 +55,10 @@ object day2Problem extends App {
     lines
   }
 
-  def areWinner(wordGuess :String): Boolean ={
-    if(wordGuess.contains("_")){
+  def areWinner(wordGuess: String): Boolean = {
+    if (wordGuess.contains("_")) {
       false
-    }else {
+    } else {
       true
     }
   }
@@ -68,18 +67,16 @@ object day2Problem extends App {
     val listOfWords = readFile
     val listSize = listOfWords.length
     val random = new Random
-    listOfWords(random.nextInt(listSize + 1))
+    listOfWords(random.nextInt(listSize))
   }
 
-  def guessLetter(word: String, input :Char): Boolean = {
-    if (word contains input){
+  def guessLetter(word: String, input: Char): Boolean = {
+    if (word contains input) {
       println(input)
       true
-    }else{
+    } else {
       false
     }
   }
-
-
 
 }
